0fe3a483bfbc7077e810c3fa1240bc2b21cefc08
JosephJohnson <josephcbrookins@icloud.com>
2022-05-22 00:13:47 -0400

    fixed bug with button press, screens now change on button press as expected. Still wierd behavior with button scaling but will identify and resolve later.

--- app.js
+++ app.js
@@ -13,6 +13,25 @@ function PageTransitions(){
             this.className += ' active-btn';
         })
     }
+
+    //what sections are active class
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selection from other button
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            //hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove('active')
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
